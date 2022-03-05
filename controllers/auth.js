14d90e3a33ca710e41e1a49c8b7686be7191234b
ddabahc <ddabahc89@gmail.com>
2022-03-05 11:40:14 -0600

    user added to database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,23 @@
-exports.createOrUpdateUser = (req, res) => {
-  res.json({
-    data: "hey you hit create-or-update-user api endpoint",
-  });
+const User = require("../models/user");
+
+exports.createOrUpdateUser = async (req, res) => {
+  const { name, picture, email } = req.user;
+
+  const user = await User.findOneAndUpdate(
+    { email },
+    { name: email.split("@")[0], picture },
+    { new: true }
+  );
+  if (user) {
+    console.log("user updated", user);
+    res.json(user);
+  } else {
+    const newUser = await new User({
+      email,
+      name: email.split("@")[0],
+      picture,
+    }).save();
+    console.log("user Created", newUser);
+    res.json(newUser);
+  }
 };
