6f6d2dd91d1fa5ca4bbff38df62913c12ab36f97
ddabahc <ddabahc89@gmail.com>
2022-03-07 19:15:28 -0600

    crud categories back end

--- server.js
+++ server.js
@@ -6,16 +6,18 @@ const cors = require("cors");
 const { readdirSync } = require("fs");
 require("dotenv").config();
 
-//app
+// app
 const app = express();
 
-//DB
+// db
 mongoose
-  .connect(process.env.DATABASE, {})
-  .then(() => console.log("DB connected"))
-  .catch((err) => console.log("DB Error => ", err));
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log("DB CONNECTION ERR", err));
 
-//middleware
+// middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
@@ -26,6 +28,4 @@ readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 // port
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port} 🚀`);
-});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
