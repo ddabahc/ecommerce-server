1b0563008c9ece7b331482b8fd79e096a4d27eec
ddabahc <ddabahc89@gmail.com>
2022-03-16 15:30:00 -0600

    products displayed in homepage

--- controllers/product.js
+++ controllers/product.js
@@ -48,8 +48,37 @@ exports.read = async (req, res) => {
 
 exports.update = async (req, res) => {
   try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const updated = await Product.findOneAndUpdate(
+      { slug: req.params.slug },
+      req.body,
+      { new: true }
+    ).exec();
+    res.json(updated);
   } catch (err) {
-    console.log("PRODUCT UPDATE ERROR", err);
-    return res.status(400).send("Product update failed");
+    console.log("PRODUCT UPDATE ERROR ----> ", err);
+    // return res.status(400).send("Product update failed");
+    res.status(400).json({
+      err: err.message,
+    });
+  }
+};
+
+exports.list = async (req, res) => {
+  try {
+    // createdAt/updatedAt, desc/asc, 3
+    const { sort, order, limit } = req.body;
+    const products = await Product.find({})
+      .populate("category")
+      .populate("subs")
+      .sort([[sort, order]])
+      .limit(limit)
+      .exec();
+
+    res.json(products);
+  } catch (err) {
+    console.log(err);
   }
 };
